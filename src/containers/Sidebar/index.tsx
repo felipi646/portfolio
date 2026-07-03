a547329e468cec85d848fc4577c4fe5f7c5ddd5a
Felipi <felipi@teste.com>
2026-07-03 13:42:18 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from "../../components/Titulo"
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/felipi646.png" />
+    <Titulo fontSize={20}>Felipi Gabriel</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from "../../components/Titulo"
+import Paragrafo from "../../components/Paragrafo"
+import { GithubSecao } from "./styles"
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore eaque
+      inventore adipisci iste veritatis a laudantium? Accusamus eius esse,
+      numquam deleniti saepe explicabo maxime quis porro molestiae ea vero
+      asperiores?
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats-sigma-five.vercel.app/api?username=felipi646&show_icons=true&theme=dracula" />
+      <img src="https://github-readme-stats-sigma-five.vercel.app/api/top-langs/?username=felipi646&layout=compact&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
